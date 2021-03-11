ac5c41a8c582f993fbfc815220e576b6254c713f
Tedesco027 <78426263+Tedesco027@users.noreply.github.com>
2021-03-11 18:34:40 -0300

    Add files via upload

---
+++ Jogo/Scripts/PausaMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PausaMenu : MonoBehaviour
+{
+    //Menu de pausa
+    public static bool GamePaused = false;
+    public GameObject pauseMenuUI;
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("menu");
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
